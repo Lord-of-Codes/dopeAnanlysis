6e97b3aece4feffca8acf9d6bd5e64f89813cabd
Lord-of-Codes <suryansh.80@gmail.com>
2018-03-30 23:28:01 +0530

    Made changes and Added Documentation

--- dope.py
+++ dope.py
@@ -29,27 +29,32 @@ if ERROR is not None:
 
 I = 0
 while I < len(MARKETS):
-    if MARKETS[I]['BaseVolume'] > 0.50 or MARKETS[I]['BaseVolume'] < 0.01 :
-        MARKETS.pop(I) #Not time efficient. But whatever. Will fix it later.
+    if MARKETS[I]['BaseVolume'] > 0.50 or MARKETS[I]['BaseVolume'] < 0.01 :  #Selecting coins whose daily trade volume is between 0.01 btc to 0.5 btc
+        del MARKETS[I] #Not time efficient. But whatever. Will fix it later.
     else:
-        I = I + 1
+        I = I + 1  #Since the (I+1)th index is not moving to (I)th place. Therefore, incrementing I
 
 J = 0
 while J < len(MARKETS):
-	ORDERS, ERROR = API.get_orders(str(MARKETS[J]['TradePairId']))
+	ORDERS, ERROR = API.get_orders(str(MARKETS[J]['TradePairId'])) 	#Fetching market orders using TradePair Id of the coins who remain in list after above loop
 	if ERROR is not None:
 		print (ERROR)
-	K = 0
-	SUMBTC = 0
-	FIRSTSELL = ORDERS['Sell'][0]['Price']
-	while K < 50:
-		if ORDERS['Sell'][K]['Total'] > 0.1:
-			MARKETS.pop(J)
+	K = 0		
+	SUMBTC = 0.0		#To count the total sum of sell orders on moving down the sell orders list
+	FIRSTSELL = ORDERS['Sell'][0]['Price']		#Ask Price. Lowest Price at which a seller is willing to sell.
+	FLAG = 0
+	while K < 100:		
+		if ORDERS['Sell'][K]['Total'] > 0.1:		#Checking for sell walls
+			del MARKETS[J]
+			FLAG = 1
 			break
-		SUMBTC = SUMBTC + ORDERS['Sell'][K]['Total']
-		if SUMBTC > 1:
-			if ORDERS['Sell'][K]['Price'] < FIRSTSELL*1.8:
-				MARKETS.pop(J)
-				break
-		K=K+1
+		SUMBTC = SUMBTC + ORDERS['Sell'][K]['Total']	#Calculating total sum
+		if SUMBTC > 0.5:
+			if ORDERS['Sell'][K]['Price'] < FIRSTSELL*1.4:		#checking if the price of coin rises by 40% on investment of 0.5btc
+				del MARKETS[J]
+				FLAG = 1
+			break
+		K=K+1		
+	if FLAG == 0:
+		J=J+1		#same concept as in line 35
 print (len(MARKETS))
